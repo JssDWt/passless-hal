da423f37f7b69fed9c2704349f6177e3093c7301
Jesse de Wit <witdejesse@hotmail.com>
2019-06-25 18:12:07 +0200

    Added HalSetupTests

---
+++ Tests/HalSetupTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+using Passless.AspNetCore.Hal.Extensions;
+using Passless.AspNetCore.Hal.Factories;
+
+namespace Tests
+{
+    public class HalSetupTests
+    {
+        private Mock<IServiceProvider> ServiceProvider => new Mock<IServiceProvider>();
+        private Mock<IHalResourceFactoryMetadata> ResourceFactory => new Mock<IHalResourceFactoryMetadata>();
+
+        [Test]
+        public void Ctor_ThrowsIfNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HalSetup(null, null));
+            Assert.Throws<ArgumentNullException>(() => new HalSetup(ServiceProvider.Object, null));
+            Assert.Throws<ArgumentNullException>(() => new HalSetup(null, ResourceFactory.Object));
+        }
+
+        [Test]
+        public void Configure_OptionsNullThrows()
+        {
+            IConfigureOptions<HalOptions> options = new HalSetup(ServiceProvider.Object, ResourceFactory.Object);
+            Assert.Throws<ArgumentNullException>(() => options.Configure(null));
+        }
+
+        [Test]
+        public void Configure_AddsDefaultResourceFactory()
+        {
+            var resourceFactory = ResourceFactory.Object;
+            IConfigureOptions<HalOptions> setup = new HalSetup(ServiceProvider.Object, resourceFactory);
+            var options = new HalOptions();
+
+            setup.Configure(options);
+            Assert.AreSame(resourceFactory, options.ResourceFactory);
+        }
+
+        [Test]
+        public void Configure_DoesNotAddDefaultResourceFactory()
+        {
+            IConfigureOptions<HalOptions> setup = new HalSetup(ServiceProvider.Object, ResourceFactory.Object);
+            var options = new HalOptions
+            {
+                UseDefaultResourceFactory = false
+            };
+
+            setup.Configure(options);
+            Assert.IsNull(options.ResourceFactory);
+        }
+
+        [Test]
+        public void Configure_DoesNotAddResourceInspectors()
+        {
+            var resourceFactory = ResourceFactory.Object;
+            IConfigureOptions<HalOptions> setup = new HalSetup(ServiceProvider.Object, resourceFactory);
+            var options = new HalOptions
+            {
+                UseDefaultResourceInspectors = false
+            };
+
+            setup.Configure(options);
+            Assert.AreEqual(0, options.ResourceInspectors.Count);
+        }
+
+        // TODO: Test adding default resourceinspectors.
+    }
+}
